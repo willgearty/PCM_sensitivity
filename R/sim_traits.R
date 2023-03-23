b2823ca7958c523db280932b1f1ecdd5f713ebde
Will Gearty <willgearty@gmail.com>
2023-03-23 11:10:57 -0400

    Fix names

--- R/sim_traits.R
+++ R/sim_traits.R
@@ -11,33 +11,27 @@ plot(tree)
 
 #BM in geiger::sim.char()
 BM_geiger <- sim.char(phy = tree, par = 0.02, nsim = 1, model = "BM", root = 1)
-names(BM_geiger) <- c(paste0('t', seq(1, 100, 1)))
-contMap(tree, BM_geiger)
+contMap(tree, BM_geiger[, , 1])
 
 #BM, BM with trend, bounded and OU in phytools::fastBM()
 #Note that internal nodes can also be estimated
 BM_phytools <- fastBM(tree, a = 1, mu = 0, sig2 = 1, bounds = c(-Inf, Inf),
                       internal = F, nsim = 1)
-names(BM_phytools) <- c(paste0('t', seq(1, 100, 1)))
 contMap(tree, BM_phytools)
 
 OU_phytools <- fastBM(tree, a = 1, sig2 = 1, alpha = 1, theta = 1, internal = F,
                       nsim = 1)
-names(OU_phytools) <- c(paste0('t', seq(1, 100, 1)))
 contMap(tree, OU_phytools)
 
 #BM, OU and ACDC / Early Burst in mvMORPH::mvSIM()
 BM_mvMORPH <- mvSIM(tree = tree, nsim = 1, model = "BM1",
                     param = list(alpha = 1, theta = 1, sigma = 0.5))
-names(BM_mvMORPH) <- c(paste0('t', seq(1, 100, 1)))
-contMap(tree, BM_mvMORPH)
+contMap(tree, BM_mvMORPH[,1])
 
 OU_mvMORPH <- mvSIM(tree = tree, nsim = 1, model = "OU1",
                     param = list(alpha = 1, theta = 1, sigma = 0.5))
-names(OU_mvMORPH) <- c(paste0('t', seq(1, 100, 1)))
-contMap(tree, OU_mvMORPH)
+contMap(tree, OU_mvMORPH[,1])
 
-EB_mvMORPH <- mvSIM(tree = tree, nsim = 1, model = "BM1",
-                    param = list(alpha = 1, theta = 1, beta = 1, sigma = 0.5))
-names(EB_mvMORPH) <- c(paste0('t', seq(1, 100, 1)))
-contMap(tree, EB_mvMORPH)
+EB_mvMORPH <- mvSIM(tree = tree, nsim = 1, model = "EB",
+                    param = list(alpha = 1, theta = 1, beta = .1, sigma = 0.5))
+contMap(tree, EB_mvMORPH[,1])
