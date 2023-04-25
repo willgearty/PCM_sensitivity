4a3de246d5b887933ade456f3ca4c00f6badc6a9
Pedro L Godoy <pedrolorenagodoy@gmail.com>
2023-04-25 13:16:16 -0300

    Code to run model-fitting analyses

--- R/analyses.R
+++ R/analyses.R
@@ -189,3 +189,126 @@ for (mod in mods) {
 
 for (mod in mods) saveRDS(get(mod), paste0("./data/", mod, ".RDS"))
 
+
+# Model fitting -------------------------------------------------------
+
+
+# loading trees
+tree_list <- readRDS("./data/tree_simulations.RDS")
+
+
+## getting rid of possible zero-length branches
+# by adding 0.00001 to zero-length branches
+
+tree_list <- lapply(tree_list, function(ntip) {
+  lapply(ntip, function(fossil_props) {
+    lapply(fossil_props, function(lambdas) {
+      lapply(lambdas, function(mus) {
+        lapply(mus, function(tree) {
+          zero <- tree$edge.length == 0
+          tree$edge.length[zero] <- tree$edge.length[zero] + 0.00001
+          tree
+        })
+      })
+    })
+  })
+})
+
+
+# loading traits
+wBM_trait  <- readRDS("./data/wBM.RDS")
+sBM_trait  <- readRDS("./data/sBM.RDS")
+
+wtrend_trait <- readRDS("./data/wtrend.RDS")
+strend_trait  <- readRDS("./data/strend.RDS")
+
+wOUc_trait  <- readRDS("./data/wOUc.RDS")
+sOUc_trait  <- readRDS("./data/sOUc.RDS")
+
+wOUs_trait  <- readRDS("./data/wOUs.RDS")
+sOUs_trait  <- readRDS("./data/sOUs.RDS")
+
+wAC_trait  <- readRDS("./data/wAC.RDS")
+sAC_trait  <- readRDS("./data/sAC.RDS")
+
+wDC_trait  <- readRDS("./data/wDC.RDS")
+sDC_trait  <- readRDS("./data/sDC.RDS")
+
+
+# create empty list to store all results
+model_fitting_results <- list()
+
+for (mod in mods) {
+
+# create lists that will be used in the loop
+assign(paste0("model_fitting_",mod), tree_list)
+simulated_traits <- get(paste0(mod,"_trait"))
+
+
+## use mvMORPH to fit 6 models (for each of the 12 sets of data simulated with the 12 models)
+
+for (i in 1:length(n_tips)) {
+  for (j in 1:length(fossil_props)) {
+     for (k in 1:length(lambdas)) {
+       for (l in 1:length(mus)) {
+         for (m in 1:n_sim) {
+          tree <- tree_list[[i]][[j]][[k]][[l]][[m]]
+          data <- simulated_traits[[i]][[j]][[k]][[l]][[m]]
+
+          # BM
+          assign(paste0("fit_", mod, "_BM"),
+                 mvBM(tree = tree, data = data,
+                    model = "BM1", method = "rpf"))
+
+          # trend
+          assign(paste0("fit_", mod, "_trend"),
+                 mvBM(tree = tree, data = data,
+                    model = "BM1", method = "rpf",
+                    param = list(trend = TRUE)))
+
+          # OU 1 theta
+          assign(paste0("fit_", mod, "_OU1"),
+                 mvOU(tree = tree, data = data,
+                    model="OU1", param=list(root=FALSE)))
+
+          # OU 2 theta
+          assign(paste0("fit_", mod, "_OU2"),
+                 mvOU(tree = tree, data = data,
+                    model="OU1", param=list(root=TRUE)))
+
+          # AC
+          assign(paste0("fit_", mod, "_AC"),
+                 mvEB(tree = tree, data = data,
+                    param=list(up=1)))
+
+          # DC
+          assign(paste0("fit_", mod, "_DC"),
+                 mvEB(tree = tree, data = data))
+
+# create a list with the results for the current dataset/model
+obj_list <- list(get(paste0("fit_", mod, "_BM")),
+                 get(paste0("fit_", mod, "_trend")),
+                 get(paste0("fit_", mod, "_OU1")),
+                 get(paste0("fit_", mod, "_OU2")),
+                 get(paste0("fit_", mod, "_AC")),
+                 get(paste0("fit_", mod, "_DC")))
+
+# assign names to the list elements
+names(obj_list) <- c("BM", "trend", "OU1", "OU2", "AC", "DC")
+
+#Replace tree with trait list
+eval(parse(text = paste0("model_fitting_", mod, "[[", i, "]]",
+                           "[[", j, "]]", "[[", k, "]]",
+                           "[[", l, "]]", "[[", m, "]] <- obj_list")))
+
+        }
+      }
+    }
+  }
+}
+
+# add the list to the results list
+model_fitting_results[[mod]] <-  get(paste0("model_fitting_", mod))
+}
+
+
